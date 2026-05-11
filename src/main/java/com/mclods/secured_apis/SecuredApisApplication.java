0a6f6b11a5bfc10c08854a80a6483afde5c5a8ca
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-05-11 22:49:11 +0530

    First Commit

---
+++ src/main/java/com/mclods/secured_apis/SecuredApisApplication.java
@@ -0,0 +1,13 @@
+package com.mclods.secured_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mclods/secured_apis/SecuredApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mclods.secured_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
